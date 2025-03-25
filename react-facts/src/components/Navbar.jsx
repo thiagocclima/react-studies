63d05f914d14c194620ae1a8a6320fcbf83b8851
Thiago Lima <thiago.lima@emergn.com>
2025-03-25 12:41:06 +0000

    new project react facts

---
+++ react-facts/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <nav >
+            <img src="./public/react-logo.png" alt="the React logo" />
+            <h1>ReactFacts</h1>
+        </nav>        
+    )
+}
