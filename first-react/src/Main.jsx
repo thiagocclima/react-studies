ad499fbf815ac7c24ed91683821d8d6c3da97ea5
Thiago Lima <thiago.lima@emergn.com>
2025-03-25 11:06:15 +0000

    styling in react and organize components

---
+++ first-react/src/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+    return (
+        <main>
+        <h1>Reason I am excited to learn React</h1>
+        <ol>
+            <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! </li>
+            <li>I would like to do something different</li>
+        </ol>
+        </main>
+    )
+}
--- first-react/src/index.jsx
+++ first-react/src/index.jsx
@@ -1,34 +1,9 @@
 import { createRoot } from 'react-dom/client'
+import Header from '../src/Header'
+import Main from '../src/Main'
+import Footer from '../src/Footer'
 
 const root = createRoot(document.getElementById('root'))
-
-function Header() {
-  return (
-      <header>
-        <img src="../public/react-logo.png" width="40px" alt="the react logo" />
-      </header>
-  )
-}
-
-function Main() {
-  return (
-    <main>
-      <h1>Reason I am excited to learn React</h1>
-      <ol>
-        <li>React is a popular library, so I will be able to fit in with all the coolest devs out there!</li>
-        <li>I would like to do something different</li>
-      </ol>
-    </main>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2024 Lima development. All rights reserved.</small>
-    </footer>
-  )
-}
 
 function Page () {
   return (
